51d8729a1e88bfab1bb5a89198b1e1a1812909bd
sujikim <sujikim@student.42seoul.kr>
2023-09-06 04:04:12 +0900

    chore: body margin 0을 위한 GlobalStyle 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="App">app</div>;
-}
+import GlobalStyles from './components/GlobalStyles';
+
+const App = () => {
+  return (
+    <main>
+      <GlobalStyles />
+      app
+    </main>
+  );
+};
 
 export default App;
---
+++ src/components/GlobalStyles.tsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+export default GlobalStyles;
