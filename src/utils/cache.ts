024cf5f674c6142292e875143959013b39ac7fed
sujikim <sujikim@student.42seoul.kr>
2023-09-08 23:01:41 +0900

    refactor: 캐시 유효기간 확인 로직 가독성 개선

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,4 +1,5 @@
-import { CACHE_KEY } from 'constants/cache';
+import { CACHE_KEY, CACHE_MAX_AGE, EXPIRE_HEADER } from 'constants/cache';
+import { BASIC_HEADERS } from 'constants/fetch';
 
 export const cache = async () => {
   const cacheStroage = await caches.open(CACHE_KEY);
@@ -8,8 +9,22 @@ export const cache = async () => {
   };
 
   const getCache = async (key: string) => await cacheStroage.match(key);
-
   const deleteCache = async (key: string) => await cacheStroage.delete(key);
 
   return { setCache, getCache, deleteCache };
+};
+
+export const setCacheExpireTime = (freshData: Response) => {
+  const expireTime = new Date().getTime() + CACHE_MAX_AGE;
+  return new Response(freshData.clone().body, {
+    headers: {
+      ...BASIC_HEADERS,
+      [EXPIRE_HEADER]: expireTime.toString(),
+    },
+  });
+};
+
+export const isFreshCache = (cachedResponse: Response) => {
+  const cachedExpire = cachedResponse.headers.get(EXPIRE_HEADER);
+  return cachedExpire && new Date().getTime() > +cachedExpire;
 };
--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,31 +1,23 @@
 import { Sick } from 'types/Sick';
-import { cache } from './cache';
-import { CACHE_MAX_AGE, EXPIRE_HEADER } from 'constants/cache';
+import { cache, isFreshCache, setCacheExpireTime } from './cache';
 import { BASE_URL, BASIC_HEADERS } from 'constants/fetch';
 
 export const fetcher = async (uri: string): Promise<Sick[]> => {
   const { getCache, setCache, deleteCache } = await cache();
   const cachedData = await getCache(uri);
-  if (cachedData) {
-    const cachedExpire = cachedData.headers.get(EXPIRE_HEADER);
-    const isFresh = cachedExpire && new Date().getTime() > +cachedExpire;
 
-    if (isFresh) return cachedData.json();
+  if (cachedData) {
+    if (isFreshCache(cachedData)) {
+      return cachedData.json();
+    }
     deleteCache(uri);
   }
 
-  const freshData = await fetch(BASE_URL + uri, {
-    headers: BASIC_HEADERS,
-  });
+  const freshData = await fetch(BASE_URL + uri, { headers: BASIC_HEADERS });
   console.info('calling api');
-  const expireTime = new Date().getTime() + CACHE_MAX_AGE;
-  const caching = new Response(freshData.clone().body, {
-    headers: {
-      ...BASIC_HEADERS,
-      [EXPIRE_HEADER]: expireTime.toString(),
-    },
-  });
-  setCache(uri, caching);
+
+  const cachingData = setCacheExpireTime(freshData);
+  setCache(uri, cachingData);
 
   return freshData.json();
 };
